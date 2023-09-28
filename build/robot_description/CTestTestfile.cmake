a19dfea2007bb7cb9f449929870d42781d24fc03
Luis Tabarez <luis.tabarez@alumnos.udg.mx>
2023-09-27 19:13:56 -0700

    First commit

---
+++ build/robot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/r2/cubot_ws/src/robot_description
+# Build directory: /home/r2/cubot_ws/build/robot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
